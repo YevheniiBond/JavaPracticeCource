c1a5b314ce9572d6e02e53d8920be8c3e3632b90
Yevhenii Bondarenko <yevhenii.bondarenko@blockchainvoucher.com>
2023-01-26 13:40:33 +0200

    new commit

--- src/test/java/java8/Section6/Lambda/LambdaUsers/TestUser.java
+++ src/test/java/java8/Section6/Lambda/LambdaUsers/TestUser.java
@@ -28,11 +28,9 @@ public class TestUser {
         User user13 = new User(40, "Rubby", 90, false, "hotel manager");
         User user14 = new User(21, "Viktoria", 61, false, "tour guide");
         User user15 = new User(21, "Lil", 40, false, "government officer ");
-
-
+        
         List<User> listOfUser = Arrays.asList(user1, user2, user3, user4, user5, user6, user7,
                 user8, user9, user10, user11, user12, user13, user14, user15);
-
 
         Predicate<User> predicateAge40 =  user -> user.age >= 40;
         Predicate<User> predicateFemales = user -> user.sex != male;
@@ -47,7 +45,6 @@ public class TestUser {
         userFilter(listOfUser, predicateAge40.and(predicateFemales));
     }
 
-
 //        userFilter(listOfUser, new UserCheck() {
 //            @Override
 //            public boolean checkCondition(User user) {
@@ -55,8 +52,6 @@ public class TestUser {
 //            }
 //        });
 //    }
-
-
     public void userFilter(List<User> listOfUser, Predicate<User> predicate) {
         for (User user : listOfUser) {
             if (predicate.test(user)) {
